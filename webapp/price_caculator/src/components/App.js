0d16e2a9307a5b330ec05fc17aa82ff5d43ffadb
renjiaq1 <jiaqing.ren@mail.utoronto.ca>
2020-09-27 07:11:26 -0400

    modified App.js and checkoutBar.js such that the checkout price calculator can create new component when a new item has been added to checkout price calculator.

--- webapp/price_caculator/src/components/App.js
+++ webapp/price_caculator/src/components/App.js
@@ -1,13 +1,60 @@
 import React from 'react';
 import CheckoutBar from './checkoutBar.js';
-import Item from './item.js';
 import MainBoard from './itemlist.js';
-import SumResult from './resultBar.js';
 import TitleName from './title.js';
 
 
 class App extends React.Component {
+    
+    state = {
+        itemList: []
+    } 
+
+    useMeWhenOnClick = (item_features) => {
+        this.setState({ 
+            itemList: this.state.itemList.concat([item_features])
+        })
+    }
+
+    calculateFinal = () => {
+        let sum = 0
+        let items;
+        for (items of this.state.itemList){
+            sum += parseInt(items[2], 10)*parseInt(items[3], 10)*parseInt(items[1], 10);
+        }
+        return sum;
+    }
+
+    deleteItem = (event) => {
+        let new_array = this.state.itemList;
+        let target;
+        for (let i =0; i < new_array.length; i++){
+            if (event.target.id == new_array[i][4]) {
+                target = i;
+                console.log(i);
+            }
+        }
+        new_array.splice(target, 1);
+
+        this.setState({itemList: new_array})
+    }
+
+    
     render() {
+        const listItems = this.state.itemList.map((items) =>
+    <div>
+        <li>
+            <div>
+                {items[0]} X {items[1]}  after discount: {parseInt(items[2], 10)*parseInt(items[3], 10)*parseInt(items[1], 10)}
+                <button type="reset" id={items[4]} onClick={this.deleteItem}>delete</button>
+            </div>
+        </li>
+        
+    </div>
+    
+    
+        );
+
         return (
             <div>
                 <div style={{marginBottom: '15px'}}>
@@ -15,19 +62,16 @@ class App extends React.Component {
                 </div>
 
                 <div className="ui container" style={{marginBottom: '15px'}}>
-                    <CheckoutBar />
+                    <CheckoutBar onSubmit={this.useMeWhenOnClick} />
                 </div>
 
                 <div style={{marginBottom: '15px'}}>
                     <MainBoard />
-                    <Item />
-                    <Item />
-                    <Item />
-                    <Item />
+                    <div>{listItems}</div>
                 </div>
                     
                 <div style={{marginBottom: '15px'}}>
-                    <SumResult />
+                    <div>total: {this.calculateFinal()}</div>
                 </div>
             </div>
         );
--- webapp/price_caculator/src/components/checkoutBar.js
+++ webapp/price_caculator/src/components/checkoutBar.js
@@ -2,24 +2,36 @@ import React from 'react';
 
 
 class CheckoutBar extends React.Component {
-    state = {term: "", price: ""};
-
+    constructor() {
+        super();
+        this.count = 0;
+    }
     
+    state = {name: "", quantity: 0, price: 0, discount: 1, id: null};
+
+
+    onButtonClick = event => {
+        this.props.onSubmit([this.state.name, this.state.quantity, this.state.price, this.state.discount, this.count]);
+        this.count += 1;
+    };
 
     render() {
         return (
                 <div className="ui segment">
                     <form className="ui form" onSubmit={e => e.preventDefault()}>
                         <label for="name"> put your item here: </label>
-                        <input type = "text" style = {{marginRight: '15px'}} value={this.state.term} onChange={e => this.setState({term: e.target.value})}/>
-                        <label for="name"> set quantity: </label>
-                        <input type = "text" style = {{marginRight: '15px'}}/>
-                        <label for="name"> set price: </label>
-                        <input type = "text" style = {{marginRight: '15px'}}/>
-                        <label for="name"> set discount rate: </label>
-                        <input type = "text" style = {{marginRight: '15px'}}/>
+                        <input type = "text" style = {{marginRight: '15px'}} value={this.state.name} onChange={e => this.setState({name: e.target.value})}/>
 
-                        <button variant="contained" className="ui button" style = {{marginTop: '15px'}} >Add</button>
+                        <label for="name"> set quantity: </label>
+                        <input type = "text" style = {{marginRight: '15px'}} value={this.state.quantity} onChange={e => this.setState({quantity: e.target.value})}/>
+
+                        <label for="name"> set price: </label>
+                        <input type = "text" style = {{marginRight: '15px'}} value={this.state.price} onChange={e => this.setState({price: e.target.value})}/>
+
+                        <label for="name"> set discount rate: </label>
+                        <input type = "text" style = {{marginRight: '15px'}} value={this.state.discount} onChange={e => this.setState({discount: e.target.value})}/>
+
+                        <button variant="contained" className="ui button" style = {{marginTop: '15px'}} onClick={this.onButtonClick}>Add</button>
                     </form>
                 </div>
                 
